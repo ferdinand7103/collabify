dddb20b8da360aa3decba4e38acaec542b30a134
Jacques7103 <ferdinand.jacques@binus.ac.id>
2023-06-13 15:37:23 +0700

    Added backend url

---
+++ src/components/Url.js
@@ -0,0 +1,30 @@
+const url = "http://localhost:8000";
+
+// User Backend
+export const url_login = url + "/token";
+export const url_signup = url + "/signup/";
+export const url_logout = url + "/logout";
+
+// Todo Backend
+export const url_add_todo = url + "/add-todo/";
+export const url_update_todo = url + "/update-todo/";
+export const url_update_todo_id = url + "/update-todo-id/";
+export const url_get_todo = url + "/get-todo/";
+export const url_get_todo_last = url + "/get-todo-last/";
+export const url_delete_todo = url + "/delete-todo/";
+export const url_delete_todo_id = url + "/delete-todo-id/";
+
+// Note Backend
+export const url_add_notes = url + "/add-notes/";
+export const url_update_notes = url + "/update-notes/";
+export const url_get_notes = url + "/get-notes/";
+export const url_get_notes_last = url + "/get-notes-last/";
+export const url_delete_notes = url + "/delete-notes/";
+
+// Map Backend
+export const url_add_map = url + "/add-map/";
+export const url_update_source = url + "/update-source/";
+export const url_update_xy = url + "/update-xy/";
+export const url_get_map = url + "/get-map/";
+export const url_get_map_last = url + "/get-map-last/";
+export const url_delete_map = url + "/delete-map/";
